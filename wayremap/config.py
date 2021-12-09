3f2abb9205f42da13c3079b2b738aa498a1911ce
Kay Gosho <ketsume0211@gmail.com>
2021-12-10 01:14:33 +0900

    done implementation

--- wayremap/config.py
+++ wayremap/config.py
@@ -26,29 +26,41 @@ class Binding:
         return 'alt' in self.remap and 'ctrl' in self.remap
 
 
-example_config = [
-    # Emacs-like key binding
-    Binding('ctrl.alt.a', [[k.KEY_LEFTCTRL, k.KEY_HOME]]),
-    Binding('ctrl.alt.e', [[k.KEY_LEFTCTRL, k.KEY_END]]),
-    Binding('ctrl.alt.h', [[k.KEY_LEFTCTRL, k.KEY_BACKSPACE]]),
-    Binding('ctrl.f', [[k.KEY_RIGHT]]),
-    Binding('ctrl.b', [[k.KEY_LEFT]]),
-    Binding('ctrl.p', [[k.KEY_UP]]),
-    Binding('ctrl.n', [[k.KEY_DOWN]]),
-    Binding('ctrl.k',
-            [[k.KEY_LEFTSHIFT, k.KEY_END], [k.KEY_LEFTCTRL, k.KEY_X]]),
-    Binding('ctrl.a', [[k.KEY_HOME]]),
-    Binding('ctrl.e', [[k.KEY_END]]),
-    Binding('ctrl.y', [[k.KEY_LEFTCTRL, k.KEY_V]]),
-    Binding('alt.f', [[k.KEY_LEFTCTRL, k.KEY_RIGHT]]),
-    Binding('alt.b', [[k.KEY_LEFTCTRL, k.KEY_LEFT]]),
-    Binding('alt.d', [[k.KEY_LEFTCTRL, k.KEY_DELETE]]),
-    Binding('ctrl.h', [[k.KEY_BACKSPACE]]),
-    Binding('ctrl.s', [[k.KEY_LEFTCTRL, k.KEY_F]]),
+@dataclass
+class WayremapConfig:
+    applications: list[str]
+    bindings: list[Binding]
 
-    # OSX-like key binding
-    Binding('alt.a', [[k.KEY_LEFTCTRL, k.KEY_A]]),
-    Binding('alt.c', [[k.KEY_LEFTCTRL, k.KEY_C]]),
-    Binding('alt.v', [[k.KEY_LEFTCTRL, k.KEY_V]]),
-    Binding('alt.x', [[k.KEY_LEFTCTRL, k.KEY_X]]),
-]
+
+example_config = WayremapConfig(
+    applications=[
+        'Brave-browser',
+        'Leafpad',
+        'firefoxdeveloperedition',
+    ],
+    bindings=[
+        # Emacs-like key binding
+        Binding('ctrl.alt.a', [[k.KEY_LEFTCTRL, k.KEY_HOME]]),
+        Binding('ctrl.alt.e', [[k.KEY_LEFTCTRL, k.KEY_END]]),
+        Binding('ctrl.alt.h', [[k.KEY_LEFTCTRL, k.KEY_BACKSPACE]]),
+        Binding('ctrl.f', [[k.KEY_RIGHT]]),
+        Binding('ctrl.b', [[k.KEY_LEFT]]),
+        Binding('ctrl.p', [[k.KEY_UP]]),
+        Binding('ctrl.n', [[k.KEY_DOWN]]),
+        Binding('ctrl.k',
+                [[k.KEY_LEFTSHIFT, k.KEY_END], [k.KEY_LEFTCTRL, k.KEY_X]]),
+        Binding('ctrl.a', [[k.KEY_HOME]]),
+        Binding('ctrl.e', [[k.KEY_END]]),
+        Binding('ctrl.y', [[k.KEY_LEFTCTRL, k.KEY_V]]),
+        Binding('alt.f', [[k.KEY_LEFTCTRL, k.KEY_RIGHT]]),
+        Binding('alt.b', [[k.KEY_LEFTCTRL, k.KEY_LEFT]]),
+        Binding('alt.d', [[k.KEY_LEFTCTRL, k.KEY_DELETE]]),
+        Binding('ctrl.h', [[k.KEY_BACKSPACE]]),
+        Binding('ctrl.s', [[k.KEY_LEFTCTRL, k.KEY_F]]),
+
+        # OSX-like key binding
+        Binding('alt.a', [[k.KEY_LEFTCTRL, k.KEY_A]]),
+        Binding('alt.c', [[k.KEY_LEFTCTRL, k.KEY_C]]),
+        Binding('alt.v', [[k.KEY_LEFTCTRL, k.KEY_V]]),
+        Binding('alt.x', [[k.KEY_LEFTCTRL, k.KEY_X]]),
+    ])
--- wayremap/main.py
+++ wayremap/main.py
@@ -3,7 +3,7 @@ import sys
 import evdev
 import uinput
 from all_keys import ALL_KEYS
-from config import Binding, example_config
+from config import Binding, WayremapConfig, example_config
 from constants import ALL_KEYS, ALT_KEYS, CTRL_KEYS, EV_KEY
 from i3ipc import Connection, Event
 from threading import Thread
@@ -22,12 +22,12 @@ def list_devices():
         print(device.path, device.name, device.phys)
 
 
-def subscribe_sway():
+def subscribe_sway(apps: list[str]):
     def on_window_focus(i3, _e):
         global is_remap_enabled
         focused = i3.get_tree().find_focused()
         app_name = focused.app_id or focused.window_class
-        is_remap_enabled = app_name == 'Leafpad'
+        is_remap_enabled = app_name in apps
         print('Remap {} for {}'.format(is_remap_enabled, app_name))
 
     sway = Connection('/run/user/1000/sway-ipc.1000.1601.sock')
@@ -35,7 +35,7 @@ def subscribe_sway():
     sway.main()
 
 
-def remap(config: list[Binding], path: str):
+def remap(bindings: list[Binding], path: str):
     global is_remap_enabled
     real_input = evdev.InputDevice(path)
     print('using device:', real_input)
@@ -49,31 +49,32 @@ def remap(config: list[Binding], path: str):
             real_input.grab()
 
             for event in real_input.read_loop():
-                if event.type == EV_KEY:
+                if not is_remap_enabled:
+                    virtual_uinput.emit((0x01, event.code), event.value)
+                elif event.type == EV_KEY:
                     if event.code in CTRL_KEYS:
                         is_ctrl = is_pressed(event.value)
                     if event.code in ALT_KEYS:
                         is_alt = is_pressed(event.value)
                     handled = False
-                    if is_remap_enabled:
-                        for binding in config:
-                            pass_ctrl = is_ctrl and binding.only_ctrl()
-                            pass_alt = is_alt and binding.only_alt()
-                            pass_ctrl_alt = is_ctrl and is_alt and binding.require_ctrl_alt(
-                            )
-                            if ((pass_ctrl or pass_alt or pass_ctrl_alt)
-                                    and not handled and event.code
-                                    == binding.get_remap_keycode()
-                                    and is_pressed(event.value)):
-                                handled = True
-                                virtual_uinput.emit(uinput.KEY_CAPSLOCK, 0)
-                                virtual_uinput.emit(uinput.KEY_LEFTALT, 0)
-                                for key_combo in binding.to:
-                                    virtual_uinput.emit_combo(key_combo)
-                                if is_ctrl:
-                                    virtual_uinput.emit(uinput.KEY_CAPSLOCK, 1)
-                                if is_alt:
-                                    virtual_uinput.emit(uinput.KEY_LEFTALT, 1)
+                    for binding in bindings:
+                        pass_ctrl = is_ctrl and binding.only_ctrl()
+                        pass_alt = is_alt and binding.only_alt()
+                        pass_ctrl_alt = is_ctrl and is_alt and binding.require_ctrl_alt(
+                        )
+                        if ((pass_ctrl or pass_alt or pass_ctrl_alt)
+                                and not handled
+                                and event.code == binding.get_remap_keycode()
+                                and is_pressed(event.value)):
+                            handled = True
+                            virtual_uinput.emit(uinput.KEY_CAPSLOCK, 0)
+                            virtual_uinput.emit(uinput.KEY_LEFTALT, 0)
+                            for key_combo in binding.to:
+                                virtual_uinput.emit_combo(key_combo)
+                            if is_ctrl:
+                                virtual_uinput.emit(uinput.KEY_CAPSLOCK, 1)
+                            if is_alt:
+                                virtual_uinput.emit(uinput.KEY_LEFTALT, 1)
                     if not handled:
                         virtual_uinput.emit((0x01, event.code), event.value)
     except KeyboardInterrupt:
@@ -85,14 +86,15 @@ def remap(config: list[Binding], path: str):
         )
 
 
-def run(config: list[Binding], path: str):
+def run(config: WayremapConfig, path: str):
     threads = list()
 
-    thread_remap = Thread(target=remap, args=(config, path))
+    thread_remap = Thread(target=remap, args=(config.bindings, path))
     threads.append(thread_remap)
     thread_remap.start()
 
-    thread_subscribe_sway = Thread(target=subscribe_sway, args=())
+    thread_subscribe_sway = Thread(target=subscribe_sway,
+                                   args=(config.applications, ))
     threads.append(thread_subscribe_sway)
     thread_subscribe_sway.start()
 
